9bdf88c804b557f7586fe9cc5ad99c5ea42074c3
sorabh <audace.sourabh@gmail.com>
2014-03-01 18:53:13 +0530

    adding basic structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylopa::Application.config.secret_token = '831eadbf7d90c7deee885d8d4d160f00a225020a797b9424371da16771ce61dee8608ab47e2eba03a09fef666921385b2a7dd04b667fd7acb3fb88438d2ad9e0'
